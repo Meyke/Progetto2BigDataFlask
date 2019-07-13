1415df41eb5d384580ec89e27857d3363a721d15
Meyke <michele.tedesco487@virgilio.it>
2019-07-13 20:29:38 +0200

    fixed stochastic doubly matrix but need more tests

--- fairProva.py
+++ fairProva.py
@@ -1,10 +1,12 @@
 import cvxpy as cp
 import numpy as np
+from sinkhorn_knopp import sinkhorn_knopp as skp
 
 #u = np.array((0.81, 0.80, 0.79, 0.78, 0.77, 0.76))
 # u = np.array((0.81, 0.80, 0.79, 0.78, 0.77, 0.0))
 
 #ATTENZIONE, PARAMETRIZZARE I GRUPPI. QUI FA :3, PERCHè HO DUE GRUPPI DA 3 (I PRIMI 3 E GLI ULTIMI 3)
+
 
 def findProbMatrix(u):
     v = np.array([1.0/(np.log(2 + i)) for i, _ in enumerate(u)])
@@ -21,12 +23,23 @@ def findProbMatrix(u):
 
     p_matrix = P.value
 
+    print("MATRICE PROBABILITA %s\n" % p_matrix)
     for i in range(p_matrix.shape[0]):
         for j in range(p_matrix.shape[1]):
             if p_matrix[i][j] < 0:
                 p_matrix[i][j] = 0
 
     p_matrix = np.around(p_matrix, decimals=4)
-    #print(p_matrix)
+
+    sk = skp.SinkhornKnopp()
+
+    ## I try to find the matrix each time by transposing it. This allows you to better adapt the values
+    ## change them just a little such that the sum is 1. Thanks to https://github.com/btaba/sinkhorn_knopp
+    ## I use 100 iterations for train the system
+    for i in range(100):
+        p_matrix = sk.fit(p_matrix.T)
+    print(np.sum(p_matrix, axis=0))
+    print(np.sum(p_matrix, axis=1))
+    print('\n %s' % p_matrix)
     return p_matrix
 
